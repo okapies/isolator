2f33f50584b8ed78c3317bf6349a76cc2f520f42
Yuta Okamoto <okapies@gmail.com>
2014-07-11 01:03:02 +0900

    add parallel mean function

--- src/main/scala/IsolatorOpenCVApp.scala
+++ src/main/scala/IsolatorOpenCVApp.scala
@@ -1,3 +1,5 @@
+import scala.testing.Benchmark
+
 import org.opencv.core.Core
 
 object IsolatorOpenCVApp extends App {
@@ -8,7 +10,9 @@ object IsolatorOpenCVApp extends App {
 
   args match {
     case Array(inFile, outFile) =>
-      loadVideo(inFile)(fs => saveImage(outFile, mean(fs)))
+      System.err.println("Elapsed: " + new Benchmark {
+        def run = saveImage(outFile, loadVideo(inFile)(mean))
+      }.runBenchmark(1))
     case _ =>
       System.err.println("Usage: isolator <input> <output>")
   }
--- src/main/scala/opencv/package.scala
+++ src/main/scala/opencv/package.scala
@@ -1,7 +1,12 @@
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+
 import org.opencv.core.{Core, CvType, Mat, Scalar, Size}
 import org.opencv.highgui.{Highgui, VideoCapture}
 
 package object opencv {
+
+  import ExecutionContext.Implicits.global
 
   def add(a: Mat, b: Mat): Mat = {
     val dst = new Mat
@@ -28,23 +33,53 @@ package object opencv {
     dst
   }
 
-  def mean(frames: Iterator[Mat]): Mat =
-    if (frames.hasNext) {
-      val head = convertTo(CvType.CV_64FC3)(frames.next)
-      val (count, out) =
-        frames.
-          map(convertTo(CvType.CV_64FC3)).
-          foldLeft((1, head)) { case ((cnt, sum), f) => (cnt + 1, add(sum, f)) }
+  def mean(size: Size)(typ: Int)(frames: Iterator[Mat]): Mat = {
+    val imdType = CvType.CV_32SC3 // TODO
 
-      divide(out, new Scalar(count, count, count))
-    } else {
-      new Mat
-    }
+    def zeros = Mat.zeros(size, imdType)
+    val (sum, cnt) =
+      frames
+        .map(convertTo(imdType))
+        .foldLeft((zeros, 0)) { case ((sum, i), m) => (add(sum, m), i + 1) }
 
-  def loadVideo[A](filename: String)(f: Iterator[Mat] => A): A = {
+    divide(sum, new Scalar(cnt, cnt, cnt))
+  }
+
+  def meanPar(batchSize: Int)(size: Size)(typ: Int)(frames: Iterator[Mat]): Mat = {
+    val imdType = CvType.CV_32SC3 // TODO
+
+    def zero = Mat.zeros(size, imdType) -> 0
+    def plus(a: (Mat, Int), b: (Mat, Int)) = add(a._1, b._1) -> (a._2 + b._2)
+
+    val partitions =
+      Iterator.continually(frames.take(batchSize).toSeq).takeWhile(_.nonEmpty)
+    val (sum, cnt) =
+      Await.result(
+        Future.traverse(partitions) { ms: Seq[Mat] =>
+          Future(ms.map(convertTo(imdType)(_) -> 1).fold(zero)(plus))
+        }.map { ps: Iterator[(Mat, Int)] =>
+          ps.fold(zero)(plus)
+        },
+        Duration.Inf)
+
+    divide(sum, new Scalar(cnt, cnt, cnt))
+  }
+
+  def loadVideo[A](filename: String)(f: Size => Int => Iterator[Mat] => A): A = {
     val cap = new VideoCapture(filename)
     try {
-      f(Iterator.continually(nextFrame(cap)).takeWhile(_ != None).map(_.get))
+      if (cap.grab()) {
+        val head = new Mat
+        cap.retrieve(head)
+        val size = head.size
+        val typ = head.`type`
+
+        f(size)(typ)(
+          Iterator.single(head)
+            ++ Iterator.continually(nextFrame(cap)).takeWhile(_ != None).map(_.get))
+      } else {
+        f(new Size)(-1)(Iterator.empty)
+      } 
     } finally {
       cap.release()
     }
